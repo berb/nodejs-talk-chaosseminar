32247f7f5355f30d4b165ce103bf88a9e8240a0e
cmichi <mich@elmueller.net>
2012-02-12 20:04:39 +0100

    added chat example

---
+++ examples/chat.js
@@ -0,0 +1,25 @@
+net = require('net');
+
+var clients = [];
+
+var server = net.createServer();
+
+server.on('connection', function (conn) {
+  console.log('new connection!');
+
+  clients.push(conn);
+  conn.write("Hello new client\n");
+
+  conn.on('data', function (data) {
+    broadcast(conn, data);
+  });
+});
+
+function broadcast(sender, data) {
+  clients.forEach(function (client) {
+    if (client !== sender)
+    	client.write(data);
+  });
+}
+
+server.listen(1337);
